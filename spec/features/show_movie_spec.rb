507466bebf6b206e3cc4a2aaced3fb30e6cb2dd8
Subrat Rout <subratnrout@gmail.com>
2015-09-09 22:34:46 -0700

    Added migration to movies and images including casts etc

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -15,6 +15,10 @@ describe "Viewing an individual movie" do
     expect(page).to have_text("$318,412,101.00")
     expect(page).to have_text(movie.description)
     expect(page).to have_text(movie.released_on)
+    expect(page).to have_text(movie.cast)
+    expect(page).to have_text(movie.director)
+    expect(page).to have_text(movie.duration)
+    expect(page).to have_selector("img[src$='#{movie.image_file_name}']")
   end
 
   it "shows the total gross if the total gross exceeds $50M" do
