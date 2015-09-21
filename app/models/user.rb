7cd254b3497461f7ef4dd018cee4d1a13fa1216f
Subrat Rout <subratnrout@gmail.com>
2015-09-20 21:09:48 -0700

    Added profile image through gravatar service

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
                     format: /\A[A-Z0-9]+\z/i,
                     uniqueness: {case_sensitive: false}
 
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
+
 end
