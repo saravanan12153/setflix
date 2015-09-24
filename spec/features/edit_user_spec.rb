b57e7f1120401614f5411645db46e9f2ec7688a9
Subrat Rout <subratnrout@gmail.com>
2015-09-24 12:06:49 -0700

    Ensure correct signed in user can edit only his account

--- spec/features/edit_user_spec.rb
+++ spec/features/edit_user_spec.rb
@@ -4,7 +4,7 @@ describe "Editing a user" do
 
   it "updates the user and shows the user's updated details" do
     user = User.create!(user_attributes)
-
+    sign_in(user)
     visit user_url(user)
     click_link 'Edit Account'
 
@@ -21,7 +21,7 @@ describe "Editing a user" do
 
   it "does not update the user if it's invalid" do
     user = User.create!(user_attributes)
-
+    sign_in(user)
     visit edit_user_url(user)
     fill_in 'Name', with: " "
     click_button "Update Account"
--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Viewing a user's profile page" do
   it "shows the user's details" do
     user = User.create!(user_attributes)
-
+    sign_in(user)
     visit user_url(user)
 
     expect(page).to have_text(user.name)
