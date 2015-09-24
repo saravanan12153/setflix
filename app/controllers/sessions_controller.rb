559661d4c75479b75802626aa6a35eeed190ab28
Subrat Rout <subratnrout@gmail.com>
2015-09-23 17:43:04 -0700

    Added create method for sign in in sessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    user = User.find_by(email: params[:email])
+    user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome back, #{user.name}!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Invalid email/password combination"
+      render :new
+    end
   end
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -11,4 +11,35 @@ describe "Signing in" do
     expect(page).to have_field("Email")
     expect(page).to have_field("Password")
   end
+
+  it "signs in the user if the email/password combination is valid" do
+    user = User.create(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign in'
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button 'Sign in'
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it "does not sign in the user if the email/password combination is invalid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "no match"
+
+    click_button 'Sign In'
+
+    expect(page).to have_text('Invalid')
+  end
 end
