ed253764e3e77c8109de6c8e1531da9d396d0830
Subrat Rout <subratnrout@gmail.com>
2015-09-29 09:51:46 -0700

    Moved favorites and genre to sidebar using content_for tag

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Viewing an individual movie" do
   it "shows the movie's details" do
     movie = Movie.create(movie_attributes)
-    
+
     visit movie_url(movie)
 
     expect(page).to have_text(movie.title)
@@ -31,5 +31,30 @@ describe "Viewing an individual movie" do
     visit movie_url(movie)
 
     expect(page).to have_text("Flop!")
+  end
+
+  it "shows the movie's fans and genres in the sidebar" do
+    movie = Movie.create!(movie_attributes)
+
+    user = User.create!(user_attributes)
+    movie.fans << user
+
+    genre = Genre.create!(name: "Action")
+    movie.genres << genre
+
+    visit movie_url(movie)
+
+    within("aside#sidebar") do
+      expect(page).to have_text(user.name)
+      expect(page).to have_text(genre.name)
+    end
+  end
+
+  it "includes the movie's title in the page title" do
+    movie = Movie.create!(movie_attributes)
+
+    visit movie_url(movie)
+
+    expect(page).to have_title("SetFlix - #{movie.title}")
   end
 end
