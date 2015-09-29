f2dfaa4f4dd7a09594bbf499ee390fb3d91dc1ba
Subrat Rout <subratnrout@gmail.com>
2015-09-28 18:35:29 -0700

    Added test for favorite and unfavorite a movie spec

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -2,4 +2,27 @@ require 'spec_helper'
 
 describe FavoritesController do
 
+  before do
+    @movie = Movie.create!(movie_attributes)
+  end
+
+  context "when not signed in" do
+
+    before do
+      session[:user_id] = nil
+    end
+
+    it "can not access create" do
+      post :create, movie_id: @movie
+
+      expect(response).o redirect_to(new_session_url)
+    end
+
+    it "can not access destroy" do
+      delete :destroy, id: 1, movie_id: @movie
+
+      expect(response).to redirect_to(new_session_url)
+    end
+  end
+
 end
